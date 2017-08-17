f53ee5668bad47e2b7cf519f15f2bbd7ee69a44c
pennennolde <m.endoh.0110@gmail.com>
2017-08-17 21:37:09 +0900

    edit sessions_controller, user_model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ActiveRecord::Base
+
+	#引数に関連するユーザーが存在すればそれを返し、存在しなければ新規に作成する
+ 	def self.find_or_create_from_auth_hash(auth_hash)
+
+ 		provider 	= auth_hash[:provider]
+    	uid 		= auth_hash[:uid]
+    	nickname 	= auth_hash[:info][:nickname]
+    	image_url 	= auth_hash[:info][:image]
+
+    	self.find_or_create_by(provider: provider, uid: uid) do |user|
+      		user.nickname = nickname
+      		user.image_url = image_url
+    	end
+ 	end
+
 end
